59f81f656666c464a19602cd664d14147aab174e
Bubble Growth <bubbleg.acesso@gmail.com>
2025-07-04 02:36:25 +0000

    adicione este pixel do google analytics <!-- Google tag (gtag.js) --> <s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
@@ -30,6 +31,19 @@ export default function RootLayout({
       >
         <main>{children}</main>
         <Toaster />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5KHQZCRJWP"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5KHQZCRJWP');
+          `}
+        </Script>
       </body>
     </html>
   );
