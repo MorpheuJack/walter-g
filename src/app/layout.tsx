3f4b60d80629fd3680dca62b1c43689f2201d2b2
Bubble Growth <bubbleg.acesso@gmail.com>
2025-07-13 18:50:56 +0000

    e matenha nossos cores oficiais como antes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="scroll-smooth">
+    <html lang="pt-BR" className="scroll-smooth dark">
       <head>
         <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
